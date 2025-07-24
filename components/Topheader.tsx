cf13f5d092ce3114b410f1ccdcf59181b8f63bd4
unknown <rakeshdhiman644>
2025-07-24 15:13:18 +0530

    fix Topheader name issue

--- components/Topheader.tsx
+++ components/Topheader.tsx
@@ -16,7 +16,6 @@ const TopHeader = () => {
                 />
             </Link>
 
-            {/* Center Information */}
             <div className="hidden md:flex items-center space-x-8">
                 {/* Working Days */}
                 <div className="flex items-center space-x-2">
